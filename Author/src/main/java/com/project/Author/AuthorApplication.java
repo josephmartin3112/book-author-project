a1799e7f98c0b378f4ea9015ad9b8a8cdb3e25bd
Joseph Martin <josephmartin3112@gmail.com>
2024-10-28 17:48:39 +0530

    before dockerizing

--- Author/src/main/java/com/project/Author/AuthorApplication.java
+++ Author/src/main/java/com/project/Author/AuthorApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@EnableMongoRepositories
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthorApplication {
--- Book/src/main/java/com/project/Book/BookApplication.java
+++ Book/src/main/java/com/project/Book/BookApplication.java
@@ -3,7 +3,9 @@ package com.project.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @SpringBootApplication
 @EnableDiscoveryClient
 public class BookApplication {
